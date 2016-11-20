2003c198a0231340afc833ec53ee88ee7c9e0fd1
Utsob Roy <uroybd@gmail.com>
2016-11-20 22:27:24 +0600

    3.3.6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='epubscraper',
-    version='3.3.4',
+    version='3.3.6',
     description='Post generator for eboipotro.github.io',
     author='Utsob Roy',
     author_email='uroybd@gmail.com',
     url='https://eboipotro.github.io/',
     py_modules = ['epubscraper'],
-    scripts=['epubscraper.py'],
+    scripts=['epubscraper'],
     install_requires=[
         'xmltodict',
         'pillow'
